11184cb67bcfac42dee8264603eab1747fd9cec3
Brian Jones <ba.jones@me.com>
2014-10-02 16:33:38 +1000

    Initial Commit

---
+++ SafeImage/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SafeImage
+//
+//  Created by Brian on 2/10/2014.
+//  Copyright (c) 2014 Grimsoft. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
